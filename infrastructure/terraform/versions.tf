5de27bd2ec64b776961af2ef44350e4cd85ce9cc
8ksZp56S <std-043-28>
2026-02-15 20:42:04 +0300

    terraform рабочий

---
+++ infrastructure/terraform/versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.87.0"
+    }
+  }
+  required_version = ">= 1.9.0"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+      }
+    bucket    = "terraform-state-momo-store-std-043-28"
+    region    = "ru-central1"
+    key       = "terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+}
